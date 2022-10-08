7e35c58babd14fb225a2b36de503780475b7451c
Arkwolf <petercrabbe92@gmail.com>
2022-10-08 17:26:44 +0800

    add weigand stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 mod io;
 mod vending;
 use color_eyre::eyre::{Result};
+use io::backend::IElectronicController;
+
+#[cfg(all(target_arch = "aarch64", target_os = "linux", target_vendor="unknown", target_env="gnu"))]
+fn generate() -> Result<Box<dyn IElectronicController>> {
+    Ok(Box::new(io::backend::mock::pi_os::Controller::new()?))
+}
+
+#[cfg(not(all(target_arch = "aarch64", target_os = "linux", target_vendor="unknown", target_env="gnu")))]
+fn generate() -> Result<Box<dyn IElectronicController>> {
+    Ok(Box::new(io::backend::mock::Controller::new()?))
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let controller = vending::controller::Controller::load_controller().await?;
-    println!("Hello, world!");
+    let mut io_controller = generate()?;
+    let mut controller = vending::controller::Controller::load_controller(&mut io_controller).await?;
+    controller.run().await?;
     Ok(())
 }
--- src/vending/controller.rs
+++ src/vending/controller.rs
@@ -1,23 +1,42 @@
+use crate::io::weigand::WeigandReader;
+
 use super::selection::Selection;
 use color_eyre::Result;
+use super::super::IElectronicController;
+use tokio::{sync::watch, task::JoinHandle};
+use super::super::io::weigand;
 
 pub struct Controller {
     drink_selection: Selection,
+    rx: tokio::sync::watch::Receiver<weigand::Weigand>,
     is_running: bool,
 }
 
 impl Controller {
-    pub async fn load_controller() -> Result<Self> {
-        Ok(Self{drink_selection: Selection::load_data().await?, is_running: true})
+    pub async fn load_controller(io_controller: &mut Box<dyn IElectronicController>) -> Result<Self> {
+        const GPIO_ZERO_LINE: u8 = 0;
+        const GPIO_ONE_LINE: u8 = 1;
+        let mut reader =
+        WeigandReader::new(GPIO_ZERO_LINE, GPIO_ONE_LINE, io_controller)?;
+        let (tx, rx) = watch::channel::<weigand::Weigand>(weigand::Weigand::new_unchecked(0));
+        let _x: JoinHandle<Result<()>> = tokio::task::spawn(async move {
+            reader.run(tx).await?;
+            Ok(())
+        });
+        Ok(Self{drink_selection: Selection::load_data().await?, rx, is_running: true})
     }
 
     pub async fn run(&mut self) -> Result<()> {
         while self.is_running {
-            // wait for either key or bypass
-            // dispense drink wait for selection or key
-            // wait for knocksensor or timeout
-            // notify of operation
-            
+            if self.rx.has_changed()? {
+                let message = self.rx.borrow_and_update();
+                // wait for either key or bypass
+                // validate key
+                // dispense drink wait for selection or key
+                // wait for knocksensor or timeout
+                // notify of operation
+            }
+
         }
         Ok(())
     }
